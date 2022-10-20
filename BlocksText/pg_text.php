61f396641be461c06db591f42475f52b5b93fee4
Jploch <jan.ploch@googlemail.com>
2022-10-20 15:52:48 +0200

    Update pg_text.php

--- BlocksText/pg_text.php
+++ BlocksText/pg_text.php
@@ -3,8 +3,8 @@
 //allow to change tag
 $modules->get('InputfieldPageGrid')->renderOptions(array('pageId' => $page->id, 'tag' => 'h2', 'tags' => 'h1 h2 h3 h4 h5 h6 p'));
 
-// textarea: remove all tags but br (keep "editor" tag for editor to work)
-$value = strip_tags(html_entity_decode($page->pg_text), '<br><editor>');
+// textarea: remove all tags but br (keep span tag for editor to work)
+$value = strip_tags(html_entity_decode($page->pg_text), '<br><span>');
 
 echo $value;
 
