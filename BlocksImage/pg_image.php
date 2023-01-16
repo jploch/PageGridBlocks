656b17efc34c57c79b510625b1590e28e89a871d
Jploch <jan.ploch@googlemail.com>
2023-01-16 13:44:34 +0100

    dfv

--- BlocksImage/pg_image.php
+++ BlocksImage/pg_image.php
@@ -1,27 +1,30 @@
-<?php namespace ProcessWire; 
-$placeholder='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+<?php namespace ProcessWire;
+
+$placeholder = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+$linkInternal = $page->pg_image_link ? $page->pg_image_link->url() : 0;
+$link = $linkInternal ? $linkInternal : $page->pg_image_link_external;
+$image = $page->getFormatted('pg_image') ;
 ?>
 
-<?php if($page->getFormatted('pg_image')) { ?>
+<?php if ($image ) { ?>
 
-<?php if($page->page_reference) { ?>
-<a href="<?= $page->page_reference->url() ?>">
+    <?php if ($link) { ?>
+        <a href="<?= $link ?>" <?= $linkInternal ? '' : 'target="blank"' ?> >
+        <?php } ?>
+
+        <img src="<?= $image ->size(0, 10)->url ?>" data-sizes="auto" data-srcset="
+            <?= $image ->size(0, 300)->url ?> 300w,
+            <?= $image ->size(0, 600)->url ?> 600w,
+            <?= $image ->size(0, 1000)->url ?> 1000w,
+            <?= $image ->size(0, 1500)->url ?> 1500w" class="lazyload pg-style-panel pg-fileupload" alt="<?= $image ->description ?>" />
+
+        <?php if ($link) { ?>
+        </a>
     <?php } ?>
 
-    <img src="<?= $page->getFormatted('pg_image')->size(10,0)->url ?>" data-sizes="auto" data-srcset="
-            <?= $page->getFormatted('pg_image')->size(300, 0)->url ?> 300w,
-            <?= $page->getFormatted('pg_image')->size(600, 0)->url ?> 600w,
-            <?= $page->getFormatted('pg_image')->size(1000, 0)->url ?> 1000w,
-            <?= $page->getFormatted('pg_image')->size(1500, 0)->url ?> 1500w,
-            <?= $page->getFormatted('pg_image')->size(2000, 0)->url ?> 2000w" class="lazyload pg-style-panel pg-fileupload" alt="<?= $page->getFormatted('pg_image')->description ?>" />
-
-    <?php if($page->page_reference) { ?>
-</a>
 <?php } ?>
 
-<?php } ?>
-
-<?php if( !($page->getFormatted('pg_image')) && $pageGrid['backend'] ) { ?>
-<img src="<?= $placeholder ?>" data-sizes="auto" data-srcset="" class="pg-fileupload pg-style-panel" />
+<?php if (!($image ) && $pageGrid['backend']) { ?>
+    <img src="<?= $placeholder ?>" data-sizes="auto" data-srcset="" class="pg-fileupload pg-style-panel" />
 
 <?php } ?>
