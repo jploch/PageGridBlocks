afa210c443fcfc85da1c1e3ad085fad905a00036
Jploch <jan.ploch@googlemail.com>
2023-01-19 13:17:02 +0100

    Update pg_text.php

--- blocks/pg_text.php
+++ blocks/pg_text.php
@@ -3,7 +3,7 @@
 //allow to change tag
 $pagegrid->renderOptions(array('page' => $page, 'tag' => 'h2', 'tags' => 'h1 h2 h3 h4 h5 h6 p'));
 
-// textarea: remove all tags but br (keep "editor" tag for editor to work)
+// textarea: remove all tags but br (keep div tag for editor to work)
 $value = strip_tags(html_entity_decode($page->pg_text), '<br><div>');
 
 echo $value;
