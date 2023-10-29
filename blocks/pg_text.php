36e23a02310d3c6ab7f17078331576f9afb0e289
Jploch <jan.ploch@googlemail.com>
2023-10-29 19:36:51 +0100

    inline edit fix

--- blocks/pg_text.php
+++ blocks/pg_text.php
@@ -2,15 +2,6 @@
 
 //allow to change tag
 $pagegrid->renderOptions(array('page' => $page, 'tag' => 'h2', 'tags' => 'h1 h2 h3 h4 h5 h6 p'));
-
-// remove all tags but br
-$value = $sanitizer->textarea(nl2br($page->pg_text), ['allowableTags' => '<br><a>']);
-
- // remove all tags but br, keep div tag for editor to work
-if($user->hasPermission('PageFrontEdit')) {
-    $value = $sanitizer->textarea(nl2br($page->pg_text), ['allowableTags' => '<div><br><a>']);
-} 
-
-echo $value;
-
 ?>
+
+<?= $page->pg_text ?>
--- blocks/pg_text_plain.php
+++ blocks/pg_text_plain.php
@@ -1,11 +1,16 @@
-<?php namespace ProcessWire;
+<?php
+
+namespace ProcessWire;
 
 //allow to change tag
 $pagegrid->renderOptions(array('page' => $page, 'tag' => 'h2', 'tags' => 'h1 h2 h3 h4 h5 h6 p'));
 
 // sanitize value, allow div for inline editor to work (field should use TextformatterEntities formatter and strip tags option)
-$value = $sanitizer->textarea($page->pg_text_plain, ['allowableTags' => '<div>']);
+$value = $sanitizer->textarea($page->pg_text_plain);
+
+if ($user->hasPermission('page-edit-front')) {
+    $value = $sanitizer->textarea($page->pg_text_plain, ['allowableTags' => '<div>']);
+}
 
 echo $value;
-
 ?>
