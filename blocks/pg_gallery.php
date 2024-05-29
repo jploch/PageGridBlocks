938de013f622f39e50e31bda2367d99aeb92f024
Jploch <jan.ploch@googlemail.com>
2024-05-29 14:46:06 +0200

    fixes for gallery blocks

--- blocks/pg_gallery.php
+++ blocks/pg_gallery.php
@@ -1,16 +1,14 @@
-<?php if ($page->pg_gallery) { ?>
 <figure class="photoswipe-item">
-    <a href="<?= $page->pg_gallery->size(0, 1000)->url(); ?>" itemprop="contentUrl" data-size="<?= $page->pg_gallery->size(0, 1000)->width(); ?>x<?= $page->pg_gallery->size(0, 1000)->height(); ?>">
-        <img src="<?= $page->pg_gallery->size(0, 300)->url(); ?>" data-srcset="<?= $page->pg_gallery->size(0, 300)->url(); ?> 300w, <?= $page->pg_gallery->size(0,600)->url(); ?> 600w" data-sizes="auto" class="lazyload photoswipe-item-content pg-fileupload" />
-    </a>
+    <?php if ($page->pg_gallery) { ?>
+        <a href="<?= $page->pg_gallery->size(0, 1000)->url(); ?>" itemprop="contentUrl" data-size="<?= $page->pg_gallery->size(0, 1000)->width(); ?>x<?= $page->pg_gallery->size(0, 1000)->height(); ?>">
+        <?php }; ?>
+        <pg-edit page="<?= $page->id ?>" field="pg_gallery">
+            <?php if ($page->pg_gallery) { ?>
+                <img src="<?= $page->pg_gallery->size(0, 300)->url(); ?>" data-srcset="<?= $page->pg_gallery->size(0, 300)->url(); ?> 300w, <?= $page->pg_gallery->size(0, 600)->url(); ?> 600w" data-sizes="auto" class="lazyload photoswipe-item-content pg-fileupload" />
+            <?php }; ?>
+        </pg-edit>
+        <?php if ($page->pg_gallery) { ?>
+        </a>
+    <?php }; ?>
     <!--     <figcaption itemprop="caption description">Image caption</figcaption>-->
 </figure>
-
-<?php } else {; ?>
-<img src="" class="pg-fileupload" />
-<?php }; ?>
-
-
-<?php 
-
-?>
