9256452545f80c91f976e4c92089cca70af7e301
Jploch <jan.ploch@googlemail.com>
2024-10-04 15:36:20 +0200

    option to automatically crop images from the image block

--- blocks/pg_image.php
+++ blocks/pg_image.php
@@ -5,7 +5,36 @@ namespace ProcessWire;
 $linkInternal = $page->pg_image_link ? $page->pg_image_link->url() : '';
 $link = $linkInternal ? $linkInternal : $page->pg_image_link_external;
 $image = $page->getFormatted('pg_image');
-// $image = $page->pg_image;
+$srcset = "";
+$sizes = [
+    [300, 0],
+    [600, 0],
+    [1000, 0],
+    [1500, 0],
+    [2000, 0],
+];
+
+//take aspect ratio field into account
+if ($image && $page->pg_image_ratio_width && $page->pg_image_ratio_height) {
+    $ratioWidth = $page->pg_image_ratio_width;
+    $ratioHeight = $page->pg_image_ratio_height;
+    $sizes = [
+        [300, 300 * $ratioHeight / $ratioWidth],
+        [600, 600 * $ratioHeight / $ratioWidth],
+        [1000, 1000 * $ratioHeight / $ratioWidth],
+        [1500, 1500 * $ratioHeight / $ratioWidth],
+        [2000, 2000 * $ratioHeight / $ratioWidth],
+    ];
+}
+
+//build srcset string from sizes array
+if ($image) {
+    foreach ($sizes as $s) {
+        $srcset .= $image->size($s[0], $s[1])->url . " $s[0]w, ";
+    }
+    if ($srcset) $srcset = substr($srcset, 0, -2);
+}
+
 ?>
 
 <?php if ($link && $image) { ?>
@@ -16,12 +45,7 @@ $image = $page->getFormatted('pg_image');
             <?php if ($image->ext == "gif" || $image->ext == "GIF") { ?>
                 <img src="<?= $image->size(10, 0, ['quality' => 1])->url ?>" data-src="<?= $image->url ?>" class="lazyload pg-media-responsive" alt="<?= $image->description ?>" />
             <?php } else { ?>
-                <img src="<?= $image->size(10, 0, ['quality' => 1])->url ?>" data-sizes="auto" data-srcset="
-            <?= $image->size(300, 0)->url ?> 300w,
-            <?= $image->size(600, 0)->url ?> 600w,
-            <?= $image->size(1000, 0)->url ?> 1000w,
-            <?= $image->size(1500, 0)->url ?> 1500w,
-            <?= $image->size(2000, 0)->url ?> 2000w" class="lazyload pg-media-responsive <?= $pagegrid->getCssClasses($page, 'img') ?>" alt="<?= $image->description ?>" />
+                <img src="<?= $image->size(10, 0, ['quality' => 1])->url ?>" data-sizes="auto" data-srcset="<?= $srcset ?>" class="lazyload pg-media-responsive <?= $pagegrid->getCssClasses($page, 'img') ?>" alt="<?= $image->description ?>" />
             <?php } ?>
         <?php } ?>
     </pg-edit>
