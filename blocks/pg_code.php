239c79f936dbcf897bfdc70e81947dca3339e810
Jploch <jan.ploch@googlemail.com>
2024-12-18 19:09:08 +0100

    fix block custom code

--- blocks/pg_code.php
+++ blocks/pg_code.php
@@ -5,8 +5,9 @@ namespace ProcessWire;
 
 <!-- Custom JS code can cause JS errors in backend, so we only load it for the frontend -->
 <?php if ($pagegrid->isBackend() && str_contains($page->pg_code, '<script')) { ?>
-<code>Custom JS code will only be executed on the frontend.</code>
-<?php return; } ?>
+    <code>Custom JS code will only be executed on the frontend.</code>
+<?php return;
+} ?>
 
 <?php if ($pagegrid->isBackend()) { ?>
     <style>
@@ -14,10 +15,16 @@ namespace ProcessWire;
             pointer-events: none !important;
         }
     </style>
+<?php } else { ?>
+    <style>
+        .pg-item.pg-code {
+            display: none !important;
+        }
+    </style>
 <?php } ?>
 
 <!-- render code -->
- <?php if($pagegrid->isBackend() && !$page->pg_code) { ?>
+<?php if ($pagegrid->isBackend() && !$page->pg_code) { ?>
     <code>Custom Code</code>
- <?php } ?>
+<?php } ?>
 <?= $page->pg_code ?>
