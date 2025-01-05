d5a798c45edb0833cde91b3a9a2a1e95f27abca3
Jploch <jan.ploch@googlemail.com>
2025-01-05 13:07:17 +0100

    block slider updated with inline SVGs for flexible styling of arrows

--- blocks/pg_slider.php
+++ blocks/pg_slider.php
@@ -17,12 +17,20 @@ $pagegrid->renderOptions(["children" => ['pg_image', 'pg_video', 'pg_editor', 'p
         </ul>
     </div>
     <div class="glide__arrows" data-glide-el="controls">
-        <button id="slide-arrow-left" class="glide__arrow glide__arrow--left slide-arrow-left pg-style-panel" data-glide-dir="<">left</button>
-        <button id="slide-arrow-right" class="glide__arrow glide__arrow--right slide-arrow-right pg-style-panel" data-glide-dir=">">right</button>
+        <button id="slide-arrow-left" class="glide__arrow glide__arrow--left slide-arrow-left pg-style-panel" data-glide-dir="<">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.4 44.2">
+                <path d="M24.2 42.1l-20-20 20-20" />
+            </svg>
+        </button>
+        <button id="slide-arrow-right" class="glide__arrow glide__arrow--right slide-arrow-right pg-style-panel" data-glide-dir=">">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.4 44.2">
+                <path d="M2.1 2.1l20 20-20 20"/>
+            </svg>
+        </button>
     </div>
     <div class="glide__bullets" data-class="glide__bullets" data-glide-el="controls[nav]">
-        <?php foreach ($page->children() as $key=>$value) { ?>
-            <button class="glide__bullet" data-glide-dir="=<?= $key ?>"></button>
+        <?php foreach ($page->children() as $key => $value) { ?>
+            <button class="glide__bullet" data-class="glide__bullet" data-glide-dir="=<?= $key ?>"></button>
         <?php }; ?>
     </div>
 </div>
