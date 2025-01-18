8d53f8df8761ee4bf54085742e64124d9dc05940
Jploch <jan.ploch@googlemail.com>
2025-01-18 17:40:06 +0100

    navigation block: support for nested links

--- blocks/pg_navigation.php
+++ blocks/pg_navigation.php
@@ -5,6 +5,18 @@ namespace ProcessWire;
 // breakpoint var when the hamburger is shown
 $showHamburger = $page->pg_navigation_showHamburger ? $page->pg_navigation_showHamburger : 640;
 $showHamburger .= 'px';
+$collapse = $page->pg_navigation_collapse;
+
+//get page links, level has to be one more because of root page
+$treeLevel = $page->pg_navigation_pages_level === '' ? 2 : $page->pg_navigation_pages_level + 1;
+$rootPage = $page->pg_navigation_pages_root ? $page->pg_navigation_pages_root : $pages->get('/');
+$pageLinksMarkup = $modules->get('BlocksNavigation')->renderPageTree($rootPage, $treeLevel, $collapse);
+
+//get custom links with depth from repeater
+$rpa = $page->pg_navigation_links;
+$repeater_structure = $modules->get('BlocksNavigation')->getDepthStructure($page, $page->rootParent);
+$customLinksMarkup = $modules->get('BlocksNavigation')->outputNestedList($repeater_structure, $rpa, $collapse);
+
 // wire('page') returns parent page
 // $page returns block page
 ?>
@@ -30,17 +42,46 @@ $showHamburger .= 'px';
 
         .pg-nav li {
             margin: 0;
+            padding: 0;
         }
 
         .pg-nav ul {
             display: block;
         }
 
-        .pg-nav a {
-            padding: 10px 0 10px 10px;
+        :where(.pg-nav) ul ul ul,
+        :where(.pg-nav) ul ul {
+            position: relative;
+            left: 0;
+            top: 0;
+            opacity: 1;
+            margin: 0 15px;
+            visibility: visible;
+        }
+
+        :where(.pg-nav) a {
+            padding: 10px 0 10px 15px;
+            display: inline-block;
+        }
+
+        :where(.pg-nav) ul ul a {
+            padding: 8px 0 8px 15px;
         }
 
         .pg-nav .hamburger {
+            display: block;
+        }
+
+        :where(.pg-nav) .nav-expand {
+            display: block;
+            opacity: 0.5;
+        }
+
+        :where(.pg-nav) li:has(> .nav-expand) ul {
+            display: none;
+        }
+
+        :where(.pg-nav) li:has(> .nav-expanded)>ul {
             display: block;
         }
 
@@ -50,7 +91,7 @@ $showHamburger .= 'px';
 <nav id="pg-nav" class="pg-nav" role="navigation">
 
     <!--mobile nav buttom-->
-    <button id="hamburger-button" class="hamburger-button hamburger hamburger--spin" type="button">
+    <button id="hamburger-button" class="hamburger-button hamburger hamburger--spin" type="button" onclick="document.querySelector('body').classList.toggle('nav-active');this.classList.toggle('is-active');">
         <span class="hamburger-box">
             <span class="hamburger-inner hamburger-<?= $page->id ?>" data-class="hamburger-<?= $page->id ?>"></span>
         </span>
@@ -58,20 +99,9 @@ $showHamburger .= 'px';
 
     <!--Main nav-->
     <div id="pg-nav-menu" class="pg-nav-menu" data-class="pg-nav-menu">
-        <ul id="nav-main" class="nav-main" data-class="nav-main">
-            <?php foreach ($pages->get('/')->children() as $p) { ?>
-                <?php $active = wire('page')->id == $p->id ? 'nav-active' : ''; ?>
-                <li class="nav-<?= $p->name ?> nav-li">
-                    <a href="<?= $p->url() ?>" class="<?= $active ?>"><?= $p->title ?></a>
-                </li>
-            <?php } ?>
-            <?php foreach ($page->pg_navigation_links as $l) { ?>
-                <?php $active = wire('page')->title == $l->pg_navigation_link_label ? 'nav-active' : ''; ?>
-                <?php $target = str_starts_with($l->pg_navigation_link, 'http') ? 'target="_blank"' : ''; ?>
-                <li class="nav-<?= $l->pg_navigation_link_label ?> nav-li">
-                    <a href="<?= $l->pg_navigation_link ?>" <?= $target ?> class="<?= $active ?>"><?= $l->pg_navigation_link_label ?></a>
-                </li>
-            <?php } ?>
+        <ul id="nav-main" class="nav-main ul-level-1" data-class="nav-main">
+            <?= $pageLinksMarkup ?>
+            <?= $customLinksMarkup ?>
         </ul>
     </div>
 
